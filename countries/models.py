22df7571bf4c10a707161b8be4f344468c37095f
Saiful Islam Auny <aunychowdhury99@gmail.com>
2025-05-05 17:55:49 +0600

    model created

--- countries/models.py
+++ countries/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Language(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    cca2 = models.CharField(max_length=2, unique=True)
+    capital = models.CharField(max_length=100, blank=True, null=True)
+    population = models.BigIntegerField()
+    region = models.CharField(max_length=100)
+    timezones = models.JSONField()
+    flag = models.URLField()
+    languages = models.ManyToManyField(Language)
+
+    def __str__(self):
+        return self.name
