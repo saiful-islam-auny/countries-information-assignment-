56a49182d1180142abfdb7e61d1e81698954c09f
Saiful Islam Auny <aunychowdhury99@gmail.com>
2025-05-05 18:11:04 +0600

    country and language api view created

--- countries/views.py
+++ countries/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Country, Language
+from .serializers import CountrySerializer, LanguageSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+
+    @action(detail=True, methods=['get'])
+    def regional(self, request, pk=None):
+        country = self.get_object()
+        others = Country.objects.filter(region=country.region).exclude(pk=country.pk)
+        serializer = self.get_serializer(others, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'], url_path='by-language/(?P<lang_code>[^/.]+)')
+    def by_language(self, request, lang_code=None):
+        countries = Country.objects.filter(languages__code=lang_code)
+        serializer = self.get_serializer(countries, many=True)
+        return Response(serializer.data)
+
+class LanguageViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
